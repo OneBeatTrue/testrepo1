0681f746e696040198115216d12049deb342c9fb
OneBeatTrue <knd-13@yandex.ru>
2022-10-09 02:15:09 +0300

    Rhombus with perimeter

---
+++ rhombus.h
@@ -0,0 +1,11 @@
+#include <cmath>
+
+
+struct rhombus {
+    double first_diagonal = 0;
+    double second_diagonal = 0;
+    double perimeter (){
+        double side = pow((first_diagonal / 2) + (second_diagonal / 2), 0.5); // добавить quad
+        return side * 4;
+    }
+};
